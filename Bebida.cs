c4c3f69066f703c0603b1523e3aa6374de1337ec
Rafael Valentini Taufer <117521021+zuzuwah@users.noreply.github.com>
2023-03-13 15:39:58 -0300

    Update Bebida.cs

--- Bebida.cs
+++ Bebida.cs
@@ -5,19 +5,21 @@ using System.Threading.Tasks;
 
 namespace M01S04
 {
+   
     public class Bebida
     {
-        public int Id { get; set; }
-        public string Refrigerante { get; set; }
-        public string Suco { get; set; }
-        public decimal MiliLitro { get; set; }
-        public string NomeBebida { get; set; }
-        public decimal ValorCompra { get; set; }
-
-        public void Comprar(){
-            Console.WriteLine($"Valor da compra do produto id {Id} alterado para {ValorCompra}");
-
-        }
-        
+    public int Id { get; set; }
+    public string Tipo { get; set; }
+    public decimal MiliLitro { get; set; }
+    public string NomeBebida { get; set; }
+    public decimal ValorCompra { get; set; }
+    
+    public void Comprar(decimal novoValorCompra)
+    {
+        ValorCompra = novoValorCompra;
+        Console.WriteLine($"Valor da compra do produto id {Id} alterado para {ValorCompra}");
     }
+    
+    }
+
 }
