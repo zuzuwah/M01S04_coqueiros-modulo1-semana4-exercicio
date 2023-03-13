e09a2f393a31edbc8daf96a38d68a2857c2aef44
Rafael Valentini Taufer <117521021+zuzuwah@users.noreply.github.com>
2023-03-13 15:47:54 -0300

    Update Suco.cs

--- Suco.cs
+++ Suco.cs
@@ -5,12 +5,20 @@ using System.Threading.Tasks;
 
 namespace M01S04
 {
-    public class Suco : Bebida
+   public class Suco : Bebida
+{
+    public string TipoCaixa { get; set; }
+    
+    public void ImprimirDados()
     {
-        public string TipoCaixa { get; set; }
-
-        public void ImprimirDados(){
-            Console.WriteLine($"O produto id {Id} é um suco é do tipo {TipoCaixa} com quantidade de MiliLitros {MiliLitro}");
-        }
+        Console.WriteLine($"O produto id {Id} é um suco do tipo {TipoCaixa} com quantidade de MiliLitros {MiliLitro}");
+    }
+    
+    public Suco(int id, string tipo, decimal miliLitro, string nomeBebida, decimal valorCompra, string tipoCaixa) 
+        : base(id, tipo, miliLitro, nomeBebida, valorCompra)
+    {
+        TipoCaixa = tipoCaixa;
+    }
+    
     }
 }
