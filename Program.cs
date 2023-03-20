c7ca7729a22187a53b1e7317eea8236db0bc709b
Rafael Valentini Taufer <117521021+zuzuwah@users.noreply.github.com>
2023-03-20 14:45:09 -0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using M01S04;
+
+Menu menu = new Menu();
+menu.Exibir();
+menu.AlterarBebida();
+menu.Remover();
+menu.ListarTodasBebidas();
+menu.ListarTodosSucos();
+menu.ListarTodosRefrigerantes();
+
+
