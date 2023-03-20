74bbb7bf9fb7829f27a826608487ef63695e24d8
Rafael Valentini Taufer <117521021+zuzuwah@users.noreply.github.com>
2023-03-20 14:41:11 -0300

    Update Bebida.cs

--- Bebida.cs
+++ Bebida.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 namespace M01S04
 {
    
-    public class Bebida
-    {
-    public int Id { get; set; }
+public class Bebida
+{
+     public int Id { get; set; }
     public string Tipo { get; set; }
     public decimal MiliLitro { get; set; }
     public string NomeBebida { get; set; }
@@ -19,7 +19,35 @@ namespace M01S04
         ValorCompra = novoValorCompra;
         Console.WriteLine($"Valor da compra do produto id {Id} alterado para {ValorCompra}");
     }
+
+        internal static Bebida Find(Func<object, bool> value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Bebida(int id, string tipo, decimal miliLitro, string nomeBebida, decimal valorCompra)
+        {
+            Id = id;
+            Tipo = tipo;
+            MiliLitro = miliLitro;
+            NomeBebida = nomeBebida;
+            ValorCompra = valorCompra;
+        }
+
+        public Bebida()
+        {
+        }
+    
+         public virtual void ImprimirDados()
+        {
+            Console.WriteLine($"O produto id {this.Id} é uma bebida com {this.MiliLitro} mL"); 
+        }
     
     }
 
+
+
 }
+
+
+
